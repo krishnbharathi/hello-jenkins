d029dc9a5bafb0ca02d6877fecb8ecaa4f469192
krishnbharathi <krishnbharathi@gmail.com>
2016-07-26 15:45:10 +0530

    new build

---
+++ samplenew/src/samplenew/GitHubClass.java
@@ -0,0 +1,12 @@
+package samplenew;
+
+public class GitHubClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Donec");
+		
+		System.out.println("master for changes ");
+	}
+
+}
