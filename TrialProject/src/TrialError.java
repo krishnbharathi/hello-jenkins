f39c203c529da3b2f986fbf72f34d97dc6ccad72
krishnbharathi <krishnbharathi@gmail.com>
2016-07-26 14:18:33 +0530

    sdfsdf

---
+++ TrialProject/src/TrialError.java
@@ -0,0 +1,10 @@
+
+public class TrialError {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("trialError 01");
+System.out.println("trialError LocalRemote");
+	}
+
+}
